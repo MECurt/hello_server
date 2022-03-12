0560d4444957d49beefe0d994f81a783a67f618e
Evan Curtis <m.e.curtis@protonmail.com>
2022-03-12 03:41:52 -0600

    Added multithreading support Added thread pool to lib.rs Removed debug print from main

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,24 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
+use hello_server::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
     let (status_line, file_name) = if buffer.starts_with(get) {
