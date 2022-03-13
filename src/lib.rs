c00d8fe10adda4df1642c336a77c66464839982c
Evan Curtis <m.e.curtis@protonmail.com>
2022-03-13 00:55:58 -0600

    Added Message enum to tell threads to shut down

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,18 @@ use std::thread;
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
-use std::thread::Thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool
@@ -37,14 +41,22 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
-        let job = Box::new(f);
+        let message = Message::NewJob(Box::new(f));
 
-        self.sender.send(job).unwrap();
+        self.sender.send(message).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers");
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
@@ -61,13 +73,22 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-            println!("Worker {} got a job. Now executing...", id);
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job. Now executing...", id);
 
-            job();
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate", id);
+
+                    break;
+                }
+            }
         });
         Worker {
             id,
